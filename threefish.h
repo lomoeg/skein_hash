ec214a2419d6aa07e65d762b48490d093d290931
DK <dknguyen.work@gmail.com>
2019-12-03 20:12:27 +0200

    first commit from Khoa

--- threefish.h
+++ threefish.h
@@ -2,6 +2,9 @@
  * You can add your own functions above, but don't modify below this line.
  */
 
+// Khoa Nguyen - 272580
+// Peter Bakholdin - 291156
+
 /* Under key at k and tweak at t, encrypt 32 bytes of plaintext at p and store it at c. */
 void threefish(unsigned char *c, const unsigned char *p, const unsigned char *k, const unsigned char *t);
 
